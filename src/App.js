ec54e0bc5ef0b5f8c2d127b41f6c18088cf3e04b
Prodip Roy <you@example.com>
2022-05-01 12:11:11 +0600

    jwt and order form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './Pages/About/About';
 import AddUser from './Pages/AddUser/AddUser';
 import Checkout from './Pages/Checkout/Checkout/Checkout';
 import Home from './Pages/Home/Home/Home';
+import { ToastContainer } from 'react-toastify';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
 import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
@@ -12,6 +13,7 @@ import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import NotFound from './Pages/Shared/NotFound/NotFound';
+import Orders from './Pages/Order/Orders';
 
 function App() {
   return (
@@ -24,7 +26,7 @@ function App() {
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
-        <Route path="/checkout" element={
+        <Route path="/checkout/:serviceId" element={
           <RequireAuth>
             <Checkout></Checkout>
           </RequireAuth>
@@ -39,9 +41,15 @@ function App() {
             <ManagService /> 
           </RequireAuth>
         }></Route>
+        <Route path='/orders' element={
+          <RequireAuth>
+            <Orders /> 
+          </RequireAuth>
+        }></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
+      <ToastContainer />
     </div>
   );
 }
--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { useParams } from 'react-router-dom'
+import useServiceDetail from '../../../hooks/useServiceDetails';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import axios from 'axios'; 
+import { toast } from 'react-toastify';
 
 const Checkout = () => {
+    const {serviceId} = useParams();
+    const [service] = useServiceDetail(serviceId)
+    const [user] = useAuthState(auth);
+    const handlePlaceOrder = e =>{
+        e.preventDefault()
+        const order = {
+            email: user.email,
+            service: service.name,
+            serviceId: serviceId,
+            address: e.target.address.value,
+            phone: e.target.phone.value
+
+        }
+
+        axios.post('http://localhost:5000/order', order)
+        .then (res =>{
+            const {data} = res;
+            if(data.insertedId){
+                toast('Your order is booked');
+                e.target.reset();
+            }
+        })
+        
+    }
     return (
-        <div>
-            <h2>Please Checkout your booking</h2>
+        <div className='w-50 mx-auto'>
+            <h2>Please Checkout: {service.name}</h2>
+
+            <form onSubmit={handlePlaceOrder}>
+                <input className='w-100 mb-2' value={user?.displayName} type="text" name='name' placeholder='Your name' required readOnly disabled/>
+                <input className='w-100 mb-2' value={user?.email} type="email" name='email' placeholder='Your email' required readOnly disabled/>
+                <input className='w-100 mb-2' value={service.name} type="text" name='service' placeholder='Your service' required readOnly/>
+                <input className='w-100 mb-2' value={user?.address} type="text" name='address' placeholder='Your address' required autoComplete='off'/>
+                <input className='w-100 mb-2' value={user?.phoneNumber} type="text" name='phone' placeholder='Your phone number' required />
+                <input className='btn btn-primary' type = 'submit' value='Place Order'></input>
+            </form>
         </div>
     );
 };
